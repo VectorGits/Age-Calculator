b88021aa74b368447ba7daf6e7348c8cd2a9fc22
VectorGits <davidoluremi0@gmail.com>
2023-09-26 18:41:56 +0100

    Fixed up file major frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        purple: 'hsl(259, 100%, 65%)',
+        lightRed: 'hsl(0, 100%, 67%)',
+        White: 'hsl(0, 0%, 100%)',
+        offWhite: 'hsl(0, 0%, 94%)',
+        lightGrey: 'hsl(0, 0%, 86%)',
+        smokeyGrey: 'hsl(0, 1%, 44%)',
+        offBlack: 'hsl(0, 0%, 8%)'
+      }
+    },
   },
   plugins: [],
 }
